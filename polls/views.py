7c116b10238936e0564dfbdfb3cf336767dd60b3
Luiz Fernando <luizfernandorg@gmail.com>
2020-07-01 19:16:35 -0300

    I created the template folder and edited the view index

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
+
+from django.template import loader
 from polls.models import Question
 #from django.shortcuts import render
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
     #return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
