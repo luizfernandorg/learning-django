0c2d638c8a6348f5aa0246e902b9e56c0c151c0d
Luiz Fernando <luizfernandorg@gmail.com>
2020-07-03 18:07:07 -0300

    updated the index view

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,17 @@
 from django.http import HttpResponse
 
-from django.template import loader
+#from django.template import loader
 from polls.models import Question
-#from django.shortcuts import render
+from django.shortcuts import render
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    #template = loader.get_template('polls/index.html')
     context = {
-        'latest_question_list': latest_question_list
+        'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+    #return HttpResponse(template.render(context,request))
     #output = ', '.join([q.question_text for q in latest_question_list])
     #return HttpResponse(output)
     #return HttpResponse("Hello, world. You're at the polls index.")
